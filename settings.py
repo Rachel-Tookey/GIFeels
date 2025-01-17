6fd5875bcffc5ada1a7d05e09d80735e079813b6
Tookey <rachel.tookey@gmail.com>
2025-01-17 00:16:22 +0000

    Feat: added in rate_limiter using redis

--- settings.py
+++ settings.py
@@ -16,3 +16,7 @@ class Config:
     SESSION_COOKIE_SAMESITE='Lax'
 
     SQLALCHEMY_DATABASE_URI=os.getenv('CONNECTION_STRING')
+
+    RATELIMIT_STORAGE_URI = "redis://127.0.0.1:6379"
+    RATELIMIT_STORAGE_OPTIONS = {"socket_connect_timeout": 30}
+    RATELIMIT_DEFAULT_LIMITS = ["200 per day", "50 per hour"]

d8d8da97253c007ea3e19f6f1b7d82cce559c3a8
Tookey <rachel.tookey@gmail.com>
2025-01-22 18:08:16 +0000

    Feat: fixed health checks in docker causing unneccesary calls

--- app/settings/cors_policy.py
+++ app/settings/cors_policy.py
@@ -1,6 +1,6 @@
 cors_resource = {
 
-    r"/*": {"origins": ["https://127.0.0.1:443", "https://www.googleapis.com/", "http://127.0.0.1:443"],
+    r"/*": {"origins": ["https://127.0.0.1:8443", "https://www.googleapis.com/", "http://127.0.0.1:8443"],
 }
 
 }

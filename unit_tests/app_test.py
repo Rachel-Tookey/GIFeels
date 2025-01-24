b7e4ee1b3afd83c1c6d1e85cbfb8b8722a39f527
Tookey <rachel.tookey@gmail.com>
2025-01-24 22:56:22 +0000

    Feat: updated tests and added comments to nginx file

--- unit_tests/app_test.py
+++ unit_tests/app_test.py
@@ -44,20 +44,12 @@ class MyTest(TestCase):
         response = self.client.get('/overview', follow_redirects=False)
         self.assert_status(response, 302)
 
-    def test_form(self):
-        response = self.client.post('/register', data={'FirstName': 'Rachel', 'LastName': 'Tookey', "Username": "Rachel1993", "email": "rachel@tookey.com", "password":"snow", "confirm":"snow", "accept_tos":True}, follow_redirects=True)
-        self.assert_200(response)
-
     def test_404(self):
         response = self.client.get('/hello')
         self.assert404(response)
 
     def test_login_200(self):
         response = self.client.get('/login')
-        self.assert_200(response, 200)
-
-    def test_login_credentials_200(self):
-        response = self.client.post('/login', data={'uname': 'JoDoe', 'password': 'password123'}, follow_redirects=True)
         self.assert_200(response, 200)
 
     def test_session(self):
